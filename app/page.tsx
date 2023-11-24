fa2cbd454f13491432484a15eed68e909f5b2466
muzammil-15 <muzammilcoder15@gmail.com>
2023-11-24 22:54:59 +0500

    main page.jsx chages

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import ReviewSection from '@/components/ReviewSection'
 import ServiceSection from '@/components/ServiceSection'
 import Image from 'next/image';
 import ModalLocation from '@/components/ModalLocation'
+import Navbar from '@/components/Navbar'
 
 const Home=async()=> {
   const accessToken = 'pk.eyJ1IjoiYWJkdWxyYTdtYW4iLCJhIjoiY2xvdnJrcDk3MHYwZjJrcWU4bHRsaTRhaSJ9.92ZDJwJAgFil80ZXA4gQcw';
